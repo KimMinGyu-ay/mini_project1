fabc2fc62f4a16acd0c2a16a128bd6126216385f
chanho <joungch2015@naver.com>
2022-01-27 13:32:05 +0900

    html css 작업

--- member/urls.py
+++ member/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('logout/', views.signout, name='user_logout'),
    
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
