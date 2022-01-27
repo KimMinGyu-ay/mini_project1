8df5fa77b0ec18dcd220238682cb22864cbf07bf
gjuu <hkjcom96@naver.com>
2022-01-28 01:27:28 +0900

    login상태표시

--- member/views.py
+++ member/views.py
@@ -3,25 +3,23 @@ from django.contrib.auth import authenticate
 from .forms import LoginForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout
 from .forms import UserForm
 
 def signup(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
+    if request.method == 'POST':        
         if request.POST['password1'] == request.POST['password2']:
             new_user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'],email=request.POST['email'])
             login(request, new_user)            
             return redirect('user_login')
         else:
             return HttpResponse('잘못 입력하였습니다')
-    else:
-        form = UserForm()
+    else:        
         return render(request, 'video/user_new.html')
 
 def signin(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username = username, password = password)
@@ -32,8 +30,7 @@ def signin(request):
             return redirect('/main')
         else:
             return HttpResponse('ERROR: Username or Password is incorrent.')
-    else:
-        form = LoginForm()
+    else:        
         return render(request,'video/user_login.html')
 
 def signout(request):
