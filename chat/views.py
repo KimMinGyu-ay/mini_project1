5f2e7c03ec9e59021a2a4daba17b94450973c6d6
sjk <klue0916@naver.com>
2022-01-28 10:59:17 +0900

    chat app 수정

--- chat/views.py
+++ chat/views.py
@@ -4,15 +4,12 @@ from django.db.models import Q
 
 # Create your views here.
 def index(request):
-    # 세션에서 로그인된 사용자 id 가져오기
-    user_id = request.session['user_id']
-    rooms = Room.objects.all()
 
-    return render(request, 'chat/index.html', {})
+    return render(request, 'chat/index.html',)
 
 def room(request, room_name):
-    user_id = request.session.get('user_id')
-    username =  User.objects.get(id = user_id).username
+    # 세션에서 로그인된 사용자 id 가져오기
+    username = request.session.get('user_id')
     messages = Message.objects.filter(room=room_name)[0:25]
 
     
